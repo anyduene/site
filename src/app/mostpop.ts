1ad48fbaaeeeb4b3e983fd641db4e0a2f7e6549c
Illia <leosmile06@gmail.com>
2023-02-14 17:49:34 +0200

    small changes

--- src/app/mostpop.ts
+++ src/app/mostpop.ts
@@ -1,7 +1,7 @@
 import { Dish } from "./dish";
 
 export const mostPopularDishes: Dish[] = [
-  { name: 'Diablo', price: 10799, img:'/assets/images/diablo.png' },
-  { name: '4-Cheese', price: 9859, img:"https://terramare.vn.ua/wp-content/uploads/2018/11/4-%D1%81%D0%B8%D1%80%D0%B0-600x600.png" },
-  { name: 'Filadelfia', price: 28499, img:"https://terramare.vn.ua/wp-content/uploads/2018/10/%D1%84%D1%96%D0%BB%D0%B0%D0%B4%D0%B5%D0%BB%D1%8C%D1%84%D1%96%D1%8F-600x600.png" }
+  { name: 'Diablo', price: 10799, img:"/assets/images/Diablo.png" },
+  { name: '4-Cheese', price: 9859, img:"/assets/images/4-Cheese.png" },
+  { name: 'Filadelfia', price: 28499, img:"/assets/images/Filadelfia.png" }
 ];
